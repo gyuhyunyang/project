0eec7fef4f61ab5dd358731445b13981bc635543
gyuhyunyang <ygh0424@naver.com>
2022-12-21 18:56:08 +0900

    12.21 업로드

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const mainMenuEl = document.querySelector('.gnb > .main-menu');
 const subMenuEl = document.querySelectorAll('.gnb .sub-menu');
 
 mainMenuEl.addEventListener('mouseover', function (){
-  headerEl.style.height = '250px';
+  headerEl.style.height = '300px';
 
   subMenuEl.forEach(function(subMenuEl) {
     subMenuEl.style.opacity='1';
@@ -22,4 +22,45 @@ mainMenuEl.addEventListener('mouseleave', function (){
 });
 
 
-// 
+// product
+const swiper = new Swiper('.product .swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 갯수  
+  spaceBetween: 20, // 슬라이드 사이 여백(간격) px
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+// notice
+const swiper = new Swiper('.notice .swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  slidesPerView: 1, // 한번에 보여줄 슬라이드 갯수  
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
