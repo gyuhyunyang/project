700a627b4f6eaa25fd85c454c8163d206dd1e7d6
gyuhyunyang <ygh0424@naver.com>
2022-12-22 14:53:32 +0900

    12.22

--- js/main.js
+++ js/main.js
@@ -42,25 +42,38 @@ const swiper = new Swiper('.product .swiper', {
   },
 });
 
+const spyEl = document.querySelectorAll('section.scroll-spy');
+spyEl.forEach(function (spyEl) {
 
-// notice
-const swiper = new Swiper('.notice .swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  slidesPerView: 1, // 한번에 보여줄 슬라이드 갯수  
-
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  new ScrollMagic.Scene ({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 감시할 요소를 지정
+    triggerHook : 0.8 // 화면의 80% 지점에서 보여짐 여부감시(0~1 사이지정)
+  })
+  .setClassToggle(spyEl, 'show')  // 요소가 화면에 보이면 show 클래스추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당 
 });
+
+
+// // notice
+// const swiper = new Swiper('.notice .swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+//   slidesPerView: 1, // 한번에 보여줄 슬라이드 갯수  
+
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+
 
 
